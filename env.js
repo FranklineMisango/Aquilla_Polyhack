32fc114315341f9429e3dc55ff2371abceba338c
misango <franklinemisango@gmail.com>
2023-06-18 21:42:28 +0800

    Test data --> API Fixed, Frontend, fix index.js

---
+++ env.js
@@ -0,0 +1,5 @@
+export const process = {
+    env: {
+        OPENAI_API_KEY: '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' //Your API keys
+    }
+}
--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-const setupTextarea = document.getElementById('setup-textarea') 
-const setupInputContainer = document.getElementById('setup-input-container')
-const movieBossText = document.getElementById('movie-boss-text')
+import { Configuration, OpenAIApi } from 'openai'
+import { process } from './env'
 
-document.getElementById("send-btn").addEventListener("click", () => {
-  if (setupTextarea.value) {
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">`
-    movieBossText.innerText = `Ok, I am thinking about that...`
-  }
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
 })
+
+const openai = new OpenAIApi(configuration)
+
+const chatbotConversation = document.getElementById('chatbot-conversation')
+ 
+let conversationStr = ''
+ 
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInput = document.getElementById('user-input') 
+    conversationStr += ` ${userInput.value} ->`
+    fetchReply()
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newSpeechBubble)
+    newSpeechBubble.textContent = userInput.value
+    userInput.value = ''
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+}) 
+
+async function fetchReply(){
+    const response = await openai.createCompletion({
+        model: 'davinci:ft-scrimba-2023-03-30-23-10-03',
+        prompt: conversationStr,
+        presence_penalty: 0,
+        frequency_penalty: 0.3,
+        max_tokens: 100,
+        temperature: 0,
+        stop: ['\n', '->']
+    })
+    conversationStr += ` ${response.data.choices[0].text} \n`
+    renderTypewriterText(response.data.choices[0].text)
+    console.log(conversationStr)
+}
+
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
+    chatbotConversation.appendChild(newSpeechBubble)
+    let i = 0
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i-1, i)
+        if (text.length === i) {
+            clearInterval(interval)
+            newSpeechBubble.classList.remove('blinking-cursor')
+        }
+        i++
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+    }, 50)
+}
